46af42d5de00420a10daed07857b0ed7e7adfa1d
fleetster <fleetster22@gmail.com>
2022-06-07 16:27:45 -0400

    another function example added

--- python essentials 1.py
+++ python essentials 1.py
@@ -131,6 +131,45 @@ def introduction(f_name, l_name):
 
 introduction(f_name = "James", l_name = "Bond")
 introduction(l_name = "Skywalker", f_name = "Luke")
+
+def is_year_leap(year):
+#
+    if (year % 400 == 0) and (year % 100 == 0) or (year % 4 ==0) and (year % 100 != 0):
+        return True
+    else:
+        return False
+#
+
+def days_in_month(year, month):
+
+    if month == 2 and is_year_leap(year):
+        return 29
+    
+    elif month == 2 and not is_year_leap(year):
+        return 28
+        
+    elif month in (1, 3, 5, 7, 8, 10, 12):
+            return 31
+        
+    elif month in (4, 6, 9, 11):
+        return 30
+        
+    else:
+        return
+#
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	result = days_in_month(yr, mo)
+	print(yr, mo, result, "->", end="")
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
 '''
     
     
