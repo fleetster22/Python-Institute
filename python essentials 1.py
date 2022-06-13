49d7d03edeaaf060ade2c8f60ace96d6f38fd21b
fleetster <fleetster22@gmail.com>
2022-06-13 15:01:34 -0400

    added couple more practice exercises

--- python essentials 1.py
+++ python essentials 1.py
@@ -170,6 +170,43 @@ for i in range(len(test_years)):
 		print("OK")
 	else:
 		print("Failed")
+  
+  
+def lb_to_kg(lb):
+    kg = round(lb * 0.45359237, 2)
+    return kg
+    
+def feet_to_meters(feet, inches):
+    meters = round((feet * 0.3048), 2) + round((inches * 0.0254), 2)
+    return meters
+
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+
+    return weight / height ** 2
+
+
+print(bmi(lb_to_kg(130), feet_to_meters(5, 6)))
+print(bmi(58.97, 1.67))
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+
+    elem_1 = elem_2 = 1
+    the_sum = 0
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+    return the_sum
+
+
+for n in range(1, 10):  # testing
+    print(n, "->", fib(n))
 '''
     
     
